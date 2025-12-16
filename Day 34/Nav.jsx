433f2fd08c4008de13bc6515cc61203e15079e70
Durgaprasad <125672017+durgaprasad-04@users.noreply.github.com>
2025-12-17 00:01:53 +0530

    Add files via upload

---
+++ Day 34/Nav.jsx
@@ -0,0 +1,16 @@
+import React  from "react";
+
+const Nav=()=>{
+    return (
+        <>
+        <div className="nav">
+            <div className="left">
+                <h2>LOGO</h2>
+            </div>
+            <div className="right">
+                <h2>Sign UP</h2>
+            </div>
+        </div>
+        </>
+    )
+}
